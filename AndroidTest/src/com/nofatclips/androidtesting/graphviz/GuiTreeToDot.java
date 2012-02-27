585d90165d75fee1d96069d05d7a370dc32cdb7d
nofatclips@gmail.com <nofatclips@gmail.com@05aa3df8-6648-ac8e-2149-f710f0335af2>
2012-02-27 21:19:57 +0000

    Updated the DOT template for the GuiTree Removed dead code

--- AndroidTest/src/com/nofatclips/androidtesting/graphviz/GuiTreeToDot.java
+++ AndroidTest/src/com/nofatclips/androidtesting/graphviz/GuiTreeToDot.java
@@ -1,8 +1,6 @@
 package com.nofatclips.androidtesting.graphviz;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 
@@ -10,11 +8,7 @@ import org.w3c.dom.Element;
 
 import com.nofatclips.androidtesting.Plottable;
 import com.nofatclips.androidtesting.guitree.GuiTree;
-import com.nofatclips.androidtesting.model.ActivityState;
-import com.nofatclips.androidtesting.model.Trace;
-import com.nofatclips.androidtesting.model.Transition;
-import com.nofatclips.androidtesting.model.UserEvent;
-import com.nofatclips.androidtesting.xml.NodeListIterator;
+import com.nofatclips.androidtesting.model.*;
 
 public class GuiTreeToDot implements Plottable {
 
@@ -25,47 +19,6 @@ public class GuiTreeToDot implements Plottable {
 		this.states = new HashMap<String,String>();
 		this.count = 0;
 	}
-	
-//	public void extractEdges () {
-//		Element session = (Element) this.session.getChildNodes().item(0);
-//		this.extractEdges (session);
-//	}
-	
-//	private void extractEdges (Element evento) {
-//		String parentName = evento.getAttribute("id");
-//		for (Element e: new NodeListIterator (evento)) {
-//			String transition = parentName + " -> " + e.getAttribute("id");
-//			this.edges.add (transition);
-//			this.extractEdges(e);
-//		}		
-//	}
-	
-//	public void extractNodes() {
-//		Element session = (Element) this.session.getChildNodes().item(0);
-//		this.extractNodes (session);
-//	}
-	
-//	private void extractNodes(Element evento) {
-//		for (Element e: new NodeListIterator (evento)) {
-//			String type = e.getAttribute("type");
-//			String name = e.getAttribute("id");
-//			String target = e.getAttribute("widget_name");
-//			boolean special = e.getAttribute("widget_type").equals("null");
-//			if (target.equals(""))
-//				target = e.getAttribute("desc");
-//			if (target.equals(""))
-//				target = e.getAttribute("value");
-//			if (target.equals("")) {
-//				target = e.getAttribute("widget_type");
-//				if (e.getAttribute("widget_id")!="") {
-//					target = target + " #" + e.getAttribute("widget_id");
-//				}
-//			}
-//			String nodeDesc = special?type:(type + "\\n'" + escapeDot(target) + "'");
-//			this.nodes.put(name, nodeDesc);
-//			this.extractNodes(e);
-//		}		
-//	}
 	
 	private String getCount() {
 		String out = "s"+this.count;
@@ -79,11 +32,10 @@ public class GuiTreeToDot implements Plottable {
 		
 		StringBuilder dot = new StringBuilder ();
 		dot.append("digraph GuiTree {" + EOL + EOL);
-		dot.append(TAB + "graph [nodesep=0.6, fontsize=36];");
+		dot.append(TAB + "graph [nodesep=1, fontsize=36];");
 
 		boolean first = true;
 		for (Trace t: this.session) {
-			String num = t.getId();
 			Transition action = t.getFinalTransition();
 			String start = action.getStartActivity().getId();
 			String finish = action.getFinalActivity().getId();
@@ -115,7 +67,7 @@ public class GuiTreeToDot implements Plottable {
 		
 		
 		dot.append(EOL + "## Edges" + EOL);
-		dot.append(EOL + TAB + "edge [fontsize=36];" + EOL + EOL);
+		dot.append(EOL + TAB + "edge [fontsize=36, headport=ne];" + EOL + EOL);
 		for (String edge:this.edges.keySet()) {
 			dot.append(TAB + edge + " [label=\"" + edges.get(edge) + "\"];" + EOL);
 		}
@@ -235,4 +187,5 @@ public class GuiTreeToDot implements Plottable {
 	private Map<String,String> nodes;
 	private Map<String,String> states;
 	private int count;
+	
 }
