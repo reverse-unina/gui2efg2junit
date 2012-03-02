c7b3e5cf6eb065d435f5f4ca27584c3adc75991a
nofatclips@gmail.com <nofatclips@gmail.com@05aa3df8-6648-ac8e-2149-f710f0335af2>
2012-03-02 13:41:17 +0000

    Updated Activity State interface to retrofit the new properties introduced in r202

--- AndroidTest/src/com/nofatclips/androidtesting/model/ActivityState.java
+++ AndroidTest/src/com/nofatclips/androidtesting/model/ActivityState.java
@@ -8,6 +8,10 @@ public interface ActivityState extends WrapperInterface, Iterable<WidgetState> {
 	public void setTitle(String title);
 	public String getId();
 	public void setId (String id);
+	public String getUniqueId();
+	public void setUniqueId (String id);
+	public String getScreenshot();
+	public void setScreenshot (String screenshot);
 	public void addWidget (WidgetState widget);
 	public boolean hasWidget (WidgetState widget);
 	
