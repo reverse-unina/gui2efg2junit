929142cc95a6b345aed7a54fc0b3481dc93a5aa5
nofatclips@gmail.com <nofatclips@gmail.com@05aa3df8-6648-ac8e-2149-f710f0335af2>
2012-07-10 10:17:09 +0000

    Added event type breakdown to the event stats in the report

--- AndroidTest/src/com/nofatclips/androidtesting/stats/InteractionStats.java
+++ AndroidTest/src/com/nofatclips/androidtesting/stats/InteractionStats.java
@@ -1,42 +1,65 @@
 package com.nofatclips.androidtesting.stats;
 
 import java.util.HashSet;
+import java.util.Hashtable;
+import java.util.Map;
 import java.util.Set;
 
 import com.nofatclips.androidtesting.model.Transition;
+import com.nofatclips.androidtesting.model.UserEvent;
 import com.nofatclips.androidtesting.model.UserInput;
 
 public class InteractionStats extends StatsReport {
 	
 	private int eventCount = 0;
 	private Set<String> events;
+	private Map<String,Integer> eventTypes;
+
 	private int inputCount = 0;
 	private Set<String> inputs;
+	private Map<String,Integer> inputTypes;
+
 	
 	public InteractionStats () {
 		this.events = new HashSet<String>();
 		this.inputs = new HashSet<String>();
+		this.eventTypes = new Hashtable<String, Integer>();
+		this.inputTypes = new Hashtable<String, Integer>();
 	}
 	
 	public void analyzeInteractions(Transition step) {
-		addEvent(step.getEvent().getId());
+		addEvent(step.getEvent());
 		this.eventCount++;
 		for (UserInput i: step) {
-			addInput(i.getId());
+			addInput(i);
 			this.inputCount++;
 		}
 	}
 
 	public Set<String> getEvents() {
-		return events;
+		return this.events;
+	}
+
+	public void addEvent (UserEvent e) {
+		if (!getEvents().contains(e.getId())) {
+			inc (this.eventTypes, e.getType());
+		}
+		addEvent (e.getId());
 	}
 
 	public void addEvent(String event) {
 		this.events.add(event);
 	}
-
+	
 	public Set<String> getInputs() {
-		return inputs;
+		return this.inputs;
+	}
+	
+	public void addInput (UserInput i) {
+		if (!getInputs().contains(i.getId())) {
+			inc (this.inputTypes, i.getType());
+		}
+		addInput (i.getId());
 	}
 
 	public void addInput(String input) {
@@ -44,11 +67,11 @@ public class InteractionStats extends StatsReport {
 	}
 
 	public int getEventCount() {
-		return eventCount;
+		return this.eventCount;
 	}
 
 	public int getInputCount() {
-		return inputCount;
+		return this.inputCount;
 	}
 	
 	public int getInteractionCount() {
@@ -72,8 +95,10 @@ public class InteractionStats extends StatsReport {
 				TAB + "different interactions: " + getDifferentInteractions() + NEW_LINE +
 				TAB + "events: " + getEventCount() + NEW_LINE +
 				TAB + "different events: " + getDifferentEvents() + NEW_LINE +
+				expandMap(this.eventTypes) +
 				TAB + "inputs: " + getInputCount() + NEW_LINE +
-				TAB + "different inputs: " + getDifferentInputs(); 
+				TAB + "different inputs: " + getDifferentInputs() + NEW_LINE +
+				expandMap(this.inputTypes);
 
 	}
 
